ea706214c708506bdfb52b07b7f67800bff980a1
diamondniam <1yungdiamond@gmail.com>
2025-08-06 16:29:52 +0400

    change: scroll to top on page change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,15 +7,17 @@ import { BrowserRouter } from "react-router";
 
 import App from "@/App.tsx";
 import { ThemeProvider } from "@mui/material";
-import { GlobalProvider } from "@/providers";
-
 import { theme } from "@/utils/styles";
+
+import { GlobalProvider } from "@/providers";
+import { PageScrollToTop } from "@/components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalProvider>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
+          <PageScrollToTop />
           <App />
         </BrowserRouter>
       </ThemeProvider>
